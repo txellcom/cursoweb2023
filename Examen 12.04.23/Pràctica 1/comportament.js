0072276f45360e35731ae4a3bdf2353d0b5be912
txellcom <114602050+txellcom@users.noreply.github.com>
2023-04-12 16:23:41 +0200

    examen
    examen

---
+++ Examen 12.04.23/Pràctica 1/comportament.js
@@ -0,0 +1,43 @@
+// var idParagraf, tamany;
+
+const miParagraf = document.getElementById("paragraf");
+
+miParagraf.addEventListener("mouseover", function() {
+  miParagraf.style.fontSize = "larger";
+});
+
+miParagraf.addEventListener("mouseout", function() {
+  miParagraf.style.fontSize = "initial";
+});
+
+// function canviEstil(idParagraf, tamany) {
+//     var paragraf = document.getElementById(idParagraf);
+//     paragraf.style.fontSize = tamany;
+//     canviarTamanyParagraf
+//     canviarTamanyParagraf(idParagraf, "35px");
+// }
+
+// function canviTamany(idParagraf) {
+//     canviarTamanyParagraf
+//     canviarTamanyParagraf(idParagraf, "35px");
+// }
+
+// function canviColor() {
+//     document.getElementById("titol").innerHTML = "¿Hola!";
+// }
+
+
+
+
+
+
+// function cambiarTamanoParrafo(idParrafo, tamano) {
+//     var parrafo = document.getElementById(idParrafo);
+//     parrafo.style.fontSize = tamano;
+//   }
+
+// function hacerMasGrande(idParrafo) {
+  
+//     cambiarTamanoParra
+//   cambiarTamanoParrafo(idParrafo, '24px');
+//   }
